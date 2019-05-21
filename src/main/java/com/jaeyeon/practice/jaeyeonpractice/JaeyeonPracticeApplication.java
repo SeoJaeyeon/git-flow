9a908c3574da7021cacb9881c13be7766fb9e50d
csbsjy <seojaeyeon@lotte.net>
2019-05-22 08:30:13 +0900

    Initial commit

---
+++ src/main/java/com/jaeyeon/practice/jaeyeonpractice/JaeyeonPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.jaeyeon.practice.jaeyeonpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeyeonPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaeyeonPracticeApplication.class, args);
+        //git init
+    }
+
+}
